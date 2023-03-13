e857b230cbdb5d1c1bd843fa4122c310239a3eec
VigilantBag <vigilantbag2018@gmail.com>
2023-03-13 15:39:22 -0700

    Add x86-64 installation script

--- installdeps.sh
+++ installdeps.sh
@@ -1,1 +1,66 @@
-#!/bin/sh
+#!/bin/bash
+
+# Prepare the system to run OpenPLC Runtime and run OpenPLC Runtime
+
+# Update the system, install dependencies, and grab required files
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git vsftpd inotify-tools
+curl https://raw.githubusercontent.com/VigilantBag/AICSHP/openplc/arm_based_installation/preconfigured_files/vsftpd.conf > vsftpd.conf
+curl https://raw.githubusercontent.com/VigilantBag/AICSHP/openplc/arm_based_installation/scripts/inotifyfilechange_arm.sh > inotifyfilechange_arm.sh
+
+# Clone the OpenPLC runtime repo
+git clone https://github.com/thiagoralves/OpenPLC_v3.git
+
+# Obtain the current user name
+echo "Enter your current user name: "
+read USER_NAME
+
+# Run OpenPLC's installation script
+cd /home/"$USER_NAME"/OpenPLC_v3
+bash /home/$USER_NAME/OpenPLC_v3/install.sh linux
+
+# Move and utilize the pre-configured vsftpd file
+cd /home/"$USER_NAME"
+sudo chmod 644 vsftpd.conf
+sudo chown root:root vsftpd.conf
+sudo rm /etc/vsftpd.conf
+sudo mv /home/"$USER_NAME"/vsftd.conf /etc/vsftpd.conf
+
+# Configure and move vsftpd.user_list file
+echo "$USER_NAME" > vsftpd.user_list
+sudo chmod 644 vsftpd.user_list
+sudo chown root:root vsftpd.user_list
+sudo mv /home/"$USER_NAME"/vsftd.user_list /etc/vsftpd.user_list
+
+# Create the ftp server
+cd /home/"$USER_NAME"/OpenPLC_v3/webserver/st_files
+
+# Restart the vsftpd
+sudo systemctl restart vsftpd
+
+# Ensure FTP traffic is allowed through the firewall
+sudo ufw allow 20:21/tcp
+sudo ufw allow 30000:31000/tcp
+
+# sudo ufw allow 8080/tcp <--uncomment for troubleshooting/setup
+# sudo ufw allow OpenSSH <-- uncomment for troubleshooting/setup
+
+# Restart the firewall to reload the ufw rules
+sudo ufw disable
+sudo ufw enable
+
+# Correct permissions, ownership and add inotify script to /etc/
+cd /home/"$USER_NAME"
+sudo chmod 755 inotifyfilechange_arm.sh
+sudo chown root:root inotifyfilechange_arm.sh
+sudo mv /home/"$USER_NAME"/inotifyfilechange_arm.sh /etc/inotifyfilechange_arm.sh
+
+# Prompt user to set up crontab
+echo ""
+echo "Add the following to crontab: "
+echo "1 * * * * @reboot bash /etc/inotifyfilechange_arm.sh"
+echo "1 * * * * :@reboot bash /etc/start_plc.sh"
+echo ""
+
+crontab -e
