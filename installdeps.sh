e08f81ca753acc21eeec935a997d899678110a61
VigilantBag <vigilantbag2018@gmail.com>
2023-03-30 08:23:45 -0700

    Add zeek install

--- installdeps.sh
+++ installdeps.sh
@@ -23,10 +23,9 @@ sudo rm /etc/vsftpd.conf
 sudo mv /home/aicshp/vsftd.conf /etc/vsftpd.conf
 
 # Configure and move vsftpd.user_list file
-echo aicshp > vsftpd.user_list
-sudo chmod 644 vsftpd.user_list
-sudo chown root:root vsftpd.user_list
-sudo mv /home/aicshp/vsftd.user_list /etc/vsftpd.user_list
+echo aicshp > /etc/vsftpd.user_list
+sudo chmod 644 /etc/vsftpd.user_list
+sudo chown root:wheel /etc/vsftpd.user_list
 
 # Create the ftp server
 cd /home/aicshp/OpenPLC_v3/webserver/st_files
@@ -38,7 +37,7 @@ sudo systemctl restart vsftpd
 sudo ufw allow 20:21/tcp
 sudo ufw allow 30000:31000/tcp
 sudo ufw allow 502
-sudo allow from any to any proto tcp port 10090:10100
+sudo ufw allow from any to any proto tcp port 10090:10100
 
 # sudo ufw allow 8080/tcp <--uncomment for troubleshooting/setup
 # sudo ufw allow OpenSSH <-- uncomment for troubleshooting/setup
@@ -50,7 +49,7 @@ sudo ufw enable
 # Correct permissions, ownership and add inotify script to /etc/
 cd /home/aicshp/
 sudo chmod 755 inotifyfilechange_arm.sh
-sudo chown root:root inotifyfilechange_arm.sh
+sudo chown root:wheel inotifyfilechange_arm.sh
 sudo mv /home/aicshp/inotifyfilechange_arm.sh /etc/inotifyfilechange_arm.sh
 sudo cp /home/aicshp/OpenPLC_v3/webserver/scripts/start_plc.sh /etc/start_plc.sh
 
