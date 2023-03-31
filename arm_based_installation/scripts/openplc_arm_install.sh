6cf3f0e9932f7cd0f382a4a613eb7fb365209a08
Owen White <owenwhite1317@gmail.com>
2023-03-30 17:48:45 -0700

    fixed openplc_arm_install.sh script & formatting

--- arm_based_installation/scripts/openplc_arm_install.sh
+++ arm_based_installation/scripts/openplc_arm_install.sh
@@ -7,7 +7,7 @@ cd /home/aicshp/
 # Update the system, install dependencies, and grab required files
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install git vsftpd inotify-tools
+sudo apt-get install -y git vsftpd inotify-tools docker.io
 wget https://raw.githubusercontent.com/VigilantBag/AICSHP/openplc/arm_based_installation/preconfigured_files/vsftpd.conf
 wget https://raw.githubusercontent.com/VigilantBag/AICSHP/openplc/arm_based_installation/scripts/inotifyfilechange_arm.sh
 
@@ -23,13 +23,13 @@ cd /home/aicshp/
 sudo chmod 644 vsftpd.conf
 sudo chown root:root vsftpd.conf
 sudo rm /etc/vsftpd.conf
-sudo mv /home/aicshp/vsftd.conf /etc/vsftpd.conf
+sudo cp /home/aicshp/vsftpd.conf /etc/vsftpd.conf
 
 # Configure and move vsftpd.user_list file
-echo AICSHP > vsftpd.user_list
-sudo chmod 644 vsftpd.user_list
-sudo chown root:root vsftpd.user_list
-sudo mv /home/aicshp/vsftd.user_list /etc/vsftpd.user_list
+echo aicshp > ./vsftpd.user_list
+sudo chmod 644 ./vsftpd.user_list
+sudo chown root:root ./vsftpd.user_list
+sudo cp /home/aicshp/vsftpd.user_list /etc/vsftpd.user_list
 
 # Create the ftp server
 cd /home/aicshp/OpenPLC_v3/webserver/st_files
@@ -43,8 +43,8 @@ sudo ufw allow 30000:31000/tcp
 sudo ufw allow 502
 sudo ufw allow 20000
 sudo ufw allow 44818
+sudo ufw allow 5601
 sudo ufw allow from any to any proto tcp port 10090:10100
-
 
 # sudo ufw allow 8080/tcp <--uncomment for troubleshooting/setup
 # sudo ufw allow OpenSSH <-- uncomment for troubleshooting/setup
@@ -55,18 +55,21 @@ sudo ufw enable
 
 # Correct permissions, ownership and add inotify script to /etc/
 cd /home/aicshp/
-sudo chmod 755 inotifyfilechange_arm.sh
-sudo chown root:root inotifyfilechange_arm.sh
-sudo mv /home/aicshp/inotifyfilechange_arm.sh /etc/inotifyfilechange_arm.sh
-sudo cp /home/aicshp/OpenPLC_v3/webserver/scripts/start_plc.sh /etc/start_plc.sh
+sudo chmod 755 ./inotifyfilechange_arm.sh
+sudo chown root:root ./inotifyfilechange_arm.sh
+sudo cp /home/aicshp/inotifyfilechange_arm.sh /etc/inotifyfilechange_arm.sh
+sudo cp /home/aicshp/OpenPLC_v3/webserver/scripts/start_openplc.sh /etc/start_openplc.sh
 
-# Prompt user to set up crontab <-- **SOMETHING IN THIS SECTION BREAKS THINGS, MUST FIX**
+# Add Zeek and Tshark Logging
+sudo groupadd docker
+sudo usermod -aG docker aicshp
+newgrp docker
+
+# Prompt user to set up crontab
 echo ""
-echo "The crontab command is: $ crontab -e "
-echo "Add the following to crontab: "
-echo "@reboot sleep 20"
-echo "@reboot sh /etc/inotifyfilechange_arm.sh"
-echo "@reboot sh /etc/start_plc.sh"
+echo "Add the following to crontab then reboot: "
+echo "1 * * * * @reboot bash /etc/inotifyfilechange_arm.sh"
+echo "1 * * * * :@reboot bash /etc/start_openplc.sh"
 echo ""
 
 sudo crontab -e
--- installdeps.sh
+++ installdeps.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
 # Prepare the system to run OpenPLC Runtime and run OpenPLC Runtime
+
+# Move to a known directory
 cd /home/aicshp/
+
 # Update the system, install dependencies, and grab required files
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y git vsftpd inotify-tools
+sudo apt-get install -y git vsftpd inotify-tools docker.io
 wget https://raw.githubusercontent.com/VigilantBag/AICSHP/openplc/arm_based_installation/preconfigured_files/vsftpd.conf
 wget https://raw.githubusercontent.com/VigilantBag/AICSHP/openplc/arm_based_installation/scripts/inotifyfilechange_arm.sh
+
 # Clone the OpenPLC runtime repo
 git clone https://github.com/thiagoralves/OpenPLC_v3.git /home/aicshp/OpenPLC_v3
 
@@ -20,13 +23,13 @@ cd /home/aicshp/
 sudo chmod 644 vsftpd.conf
 sudo chown root:root vsftpd.conf
 sudo rm /etc/vsftpd.conf
-sudo mv /home/aicshp/vsftpd.conf /etc/vsftpd.conf
+sudo cp /home/aicshp/vsftpd.conf /etc/vsftpd.conf
 
 # Configure and move vsftpd.user_list file
 echo aicshp > ./vsftpd.user_list
 sudo chmod 644 ./vsftpd.user_list
 sudo chown root:root ./vsftpd.user_list
-sudo mv ./vsftpd.user_list /etc/vsftpd.user_list
+sudo cp /home/aicshp/vsftpd.user_list /etc/vsftpd.user_list
 
 # Create the ftp server
 cd /home/aicshp/OpenPLC_v3/webserver/st_files
@@ -38,8 +41,10 @@ sudo systemctl restart vsftpd
 sudo ufw allow 20:21/tcp
 sudo ufw allow 30000:31000/tcp
 sudo ufw allow 502
-sudo ufw allow from any to any proto tcp port 10090:10100
+sudo ufw allow 20000
+sudo ufw allow 44818
 sudo ufw allow 5601
+sudo ufw allow from any to any proto tcp port 10090:10100
 
 # sudo ufw allow 8080/tcp <--uncomment for troubleshooting/setup
 # sudo ufw allow OpenSSH <-- uncomment for troubleshooting/setup
@@ -50,15 +55,16 @@ sudo ufw enable
 
 # Correct permissions, ownership and add inotify script to /etc/
 cd /home/aicshp/
-sudo chmod 755 inotifyfilechange_arm.sh
-sudo chown root:root inotifyfilechange_arm.sh
-sudo mv /home/aicshp/inotifyfilechange_arm.sh /etc/inotifyfilechange_arm.sh
+sudo chmod 755 ./inotifyfilechange_arm.sh
+sudo chown root:root ./inotifyfilechange_arm.sh
+sudo cp /home/aicshp/inotifyfilechange_arm.sh /etc/inotifyfilechange_arm.sh
 sudo cp /home/aicshp/OpenPLC_v3/webserver/scripts/start_openplc.sh /etc/start_openplc.sh
 
 # Add Zeek and Tshark Logging
 sudo groupadd docker
 sudo usermod -aG docker aicshp
 newgrp docker
+
 # Prompt user to set up crontab
 echo ""
 echo "Add the following to crontab then reboot: "
