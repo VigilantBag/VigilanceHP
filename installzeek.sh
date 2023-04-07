040070d1f315103b9c7a03425bb66ad7adf6ae8c
VigilantBag <vigilantbag2018@gmail.com>
2023-04-07 09:45:28 -0700

    Have docker container start detached

--- installzeek.sh
+++ installzeek.sh
@@ -12,8 +12,7 @@ else
     docker run -d --name kibana -p 5601:5601 --link elasticsearch -e xpack.reporting.enabled=false blacktop/kibana:x-pack-7.4.0
     echo Waiting for Kibana to start...
     sleep 3m
-    ethernet=ip -br l | awk '$1 !~ "lo|vir|wl|docker" { print $1}'
-    docker run --init --rm -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 -e
-    docker run --rm --cap-add=NET_RAW --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::$ethernet local
+    docker run -d --init --rm -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 -e
+    docker run -d --rm --cap-add=NET_RAW --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::ens18 local
     echo Kibana available on localhost:5601
 fi
