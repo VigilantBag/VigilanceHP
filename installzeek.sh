070c44f34daf6cc6e69f84efa09a10343fc61b28
VigilantBag <vigilantbag2018@gmail.com>
2023-04-10 09:37:47 -0700

    prevent autoremoval

--- installzeek.sh
+++ installzeek.sh
@@ -12,8 +12,8 @@ else
     docker run -d --name kibana -p 5601:5601 --link elasticsearch -e xpack.reporting.enabled=false blacktop/kibana:x-pack-7.4.0 --restart=always
     echo Waiting for Kibana to start...
     sleep 3m
-    docker run -d --init --rm -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 --restart=always -e
-    docker run -d --rm --cap-add=NET_RAW --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::ens18 --restart=always local
+    docker run -d --init -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 --restart=always -e
+    docker run -d --cap-add=NET_RAW --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::ens18 --restart=always local
     echo Kibana available on localhost:5601
     docker update --restart unless-stopped $(docker ps -q)
 fi
