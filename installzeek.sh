727d0d8952af522916dec59fe73c821fe0c5b1f7
VigilantBag <vigilantbag2018@gmail.com>
2023-04-10 11:15:12 -0700

    increase wait for elasticsearch

--- installzeek.sh
+++ installzeek.sh
@@ -8,12 +8,13 @@ then
 else
     git clone https://github.com/VigilantBag/ICSPOT/ -b openplc
     cd ICSPOT/Logging/
-    docker run -d --name elasticsearch -p 9200:9200 -e discovery.type=single-node blacktop/elasticsearch:x-pack-7.4.0 --restart=always
-    docker run -d --name kibana -p 5601:5601 --link elasticsearch -e xpack.reporting.enabled=false blacktop/kibana:x-pack-7.4.0 --restart=always
+    docker run -d --name elasticsearch -p 9200:9200 -e discovery.type=single-node blacktop/elasticsearch:x-pack-7.4.0 
+    sleep 30s
+    docker run -d --name kibana -p 5601:5601 --link elasticsearch -e xpack.reporting.enabled=false blacktop/kibana:x-pack-7.4.0 
     echo Waiting for Kibana to start...
     sleep 3m
-    docker run -d --init -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 --restart=always -e
-    docker run -d --cap-add=NET_RAW --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::ens18 --restart=always local
+    docker run -d --init -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 -e
+    docker run -d --cap-add=NET_RAW --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::ens18 local
     echo Kibana available on localhost:5601
     docker update --restart unless-stopped $(docker ps -q)
 fi
