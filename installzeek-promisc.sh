65f8189f1b448cf2ed72e65f273e462ac15cbb9f
VigilantBag <vigilantbag2018@gmail.com>
2023-04-11 08:28:59 -0700

    remove restart reminder from promisc

--- installzeek-promisc.sh
+++ installzeek-promisc.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
-echo ONLY RUN AFTER YOU REBOOTED FROM installdeps.sh
-echo Continue? \(y\/n\)
-read confirm
-if [ $confirm = "n" ]
-then
-    exit
-else
-    sudo groupadd docker
-    sudo usermod -aG docker aicshp
-    newgrp docker
-    git clone https://github.com/VigilantBag/ICSPOT/ -b openplc
-    cd ICSPOT/Logging/
-    docker run -d --cap-add=NET_ADMIN --name elasticsearch -p 9200:9200 -e discovery.type=single-node blacktop/elasticsearch:x-pack-7.4.0 
-    sleep 30s
-    docker run -d --cap-add=NET_ADMIN --name kibana -p 5601:5601 --link elasticsearch -e xpack.reporting.enabled=false blacktop/kibana:x-pack-7.4.0 
-    echo Waiting for Kibana to start...
-    sleep 3m
-    docker run -d --cap-add=NET_ADMIN --init -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 -e
-    docker run -d --cap-add=NET_ADMIN --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::ens18 local
-    echo Kibana available on localhost:5601
-    docker update --restart unless-stopped $(docker ps -q)
-fi
+sudo groupadd docker
+sudo usermod -aG docker aicshp
+newgrp docker
+git clone https://github.com/VigilantBag/ICSPOT/ -b openplc
+cd ICSPOT/Logging/
+docker run -d --cap-add=NET_ADMIN --name elasticsearch -p 9200:9200 -e discovery.type=single-node blacktop/elasticsearch:x-pack-7.4.0 
+sleep 30s
+docker run -d --cap-add=NET_ADMIN --name kibana -p 5601:5601 --link elasticsearch -e xpack.reporting.enabled=false blacktop/kibana:x-pack-7.4.0 
+echo Waiting for Kibana to start...
+sleep 3m
+docker run -d --cap-add=NET_ADMIN --init -it -v `pwd`:/pcap --link kibana --link elasticsearch blacktop/filebeat:7.4.0 -e
+docker run -d --cap-add=NET_ADMIN --net=host -v `pwd`:/pcap:rw blacktop/zeek:elastic -i af_packet::ens18 local
+echo Kibana available on localhost:5601
+docker update --restart unless-stopped $(docker ps -q)
