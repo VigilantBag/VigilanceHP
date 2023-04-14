61b90fbd6bde8c1b2b245fa64fcb4721db4aa672
VigilantBag <vigilantbag2018@gmail.com>
2023-04-14 09:47:18 -0700

    fix cronjob to actually run

--- installdeps.sh
+++ installdeps.sh
@@ -75,7 +75,9 @@ sudo /sbin/iptables-save | sudo tee /etc/iptables/rules.v4
 sudo systemctl enable docker.service
 
 # Prompt user to set up crontab
-echo "1 * * * * @reboot root /etc/inotifyfilechange_arm.sh" | sudo tee -a /var/spool/cron/crontabs/root
-echo "1 * * * * @reboot root /etc/start_openplc.sh" | sudo tee -a /var/spool/cron/crontabs/root
+echo "@reboot sleep 30s && sh /etc/inotifyfilechange_arm.sh > /dev/null" | sudo tee -a /var/spool/cron/crontabs/root
+echo "" | sudo tee -a /var/spool/cron/crontabs/root
+echo "@reboot sleep 30s && sh /etc/start_openplc.sh > /dev/null" | sudo tee -a /var/spool/cron/crontabs/root
+echo "" | sudo tee -a /var/spool/cron/crontabs/root
 echo "net.ipv4.conf.all.forwarding=1" | sudo tee -a /etc/sysctl.conf
 exit
